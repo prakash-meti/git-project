b0599856299d99d36c8d5da3df8a4ffd3f5db507
prakash patil <prakashpatilmeti@gmail.com>
2024-02-24 14:32:13 +0530

    update changes

--- testapp/views.py
+++ testapp/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_home_page(request):
-    return render(request, 'testapp/home.html')
+
+    context = {
+        'name':'Prakash'
+    }
+    return render(request, 'testapp/home.html', context)
